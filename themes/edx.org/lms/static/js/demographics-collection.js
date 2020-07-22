c7b389a9c14977c273db6a43ed673d6c5e824ec0
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-07-22 16:00:22 -0400

    Added the javascript to call the demographics api and hide cta

--- themes/edx.org/lms/static/js/demographics-collection.js
+++ themes/edx.org/lms/static/js/demographics-collection.js
@@ -1,1 +1,25 @@
-console.log("Demographics javascript file loaded")
+console.log('Demographics javascript file loaded');
+
+$(document).ready(function() {
+    'use strict';
+
+    $('#demographics-dismiss').click(function(event) {
+        event.preventDefault();
+        return $.ajax({
+            url: '/api/demographics/v1/demographics/status/',
+            type: 'PATCH',
+            headers: {
+                'X-CSRFToken': $.cookie('csrftoken')
+            },
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                call_to_action_dismissed: true
+            }),
+            context: this,
+            success: function() {
+                $('#demographics-banner').hide();
+            }
+        });
+    });
+});
